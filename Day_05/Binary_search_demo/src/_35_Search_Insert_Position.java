e0c1a7dc1066cec0a4f43611bb60c0bd46f281d8
Lê Đăng Quang <81614913+ldqgithub12@users.noreply.github.com>
2022-12-18 23:02:09 +0700

    update btvn
    Sửa bài search insert dùng binary search

--- Day_05/Binary_search_demo/src/_35_Search_Insert_Position.java
+++ Day_05/Binary_search_demo/src/_35_Search_Insert_Position.java
@@ -1,16 +1,38 @@
 public class _35_Search_Insert_Position {
     public static int searchInsert(int[] nums, int target) {
         int n = nums.length;
-        if (nums[n-1] < target) return n;
-        for (int i =0;i<n;i++){
-            if (nums[i] == target){
-                return i;
+//        if (nums[n-1] < target) return n;
+//        for (int i =0;i<n;i++){
+//            if (nums[i] == target){
+//                return i;
+//            }
+//            else if (target < nums[i]){
+//                return i;
+//            }
+//        }
+//        return 0;
+        int left=0;
+        int right=n;
+        int mid=(left+right)/2;
+        if(n == 0) return 0;
+        if(nums[left] > target) return 0;
+        if(nums[right-1] < target) return n;
+        while(left < right){
+            if(nums[mid] > target){
+                right = mid;
+                mid = (left+right)/2;
+            }else if(nums[mid] < target){
+                left = mid;
+                mid = (left+right)/2;
+            }else{
+                return mid;
             }
-            else if (target < nums[i]){
-                return i;
+            if(left == mid){
+                if(nums[left] == target)return left;
+                else return right;
             }
         }
-        return 0;
+        return -1;
     }
     public static void main(String[] args) {
         int[] nums = {1,3,5,6};
