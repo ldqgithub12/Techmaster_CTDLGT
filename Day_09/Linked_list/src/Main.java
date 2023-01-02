402263817c24f3f5b1c5ddcfddd3484009745ee0
quang.ledang <quang.ledang@mobifone.vn>
2023-01-02 15:23:14 +0700

    Day08

---
+++ Day_09/Linked_list/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    static class Node{
+        int val;
+        Node next;
+
+        Node(int val){
+            this.val = val;
+        }
+    }
+}
